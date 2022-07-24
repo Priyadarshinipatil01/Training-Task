88a65b0b0bcbbaa692513c16404de2a7d649f09d
PriyadarshiniPatil <48160336+Priyadarshinipatil01@users.noreply.github.com>
2022-07-24 20:19:05 +0530

    Add files via upload

---
+++ Digital-Clock/clock.js
@@ -0,0 +1,33 @@
+function showTime(){
+var date =new Date();
+var h= date.getHours();
+var m= date.getMinutes();
+var s= date.getSeconds();
+var session = "AM";
+
+if(h==0){
+    h =12; // 12 hrs format
+}
+if(h>12){
+    h=h-12;
+    session = "PM";
+}
+// if(h<10){  //0-9
+//     h="0" +h;
+// }
+// if(m<10){
+//     m="0" +m;
+// }
+// if(s<10){
+//     s="0"+s;
+// }
+
+// Ternory operator
+
+h=(h<10)?"0"+h:h;
+m=(m<10)?"0" +m:m;
+s=(s<10)?"0" +s:s;
+document.getElementById("DigitalClock").innerHTML = h + ":" + m + ":" + s + "" + session;
+setTimeout(showTime, 1000);
+}
+showTime();
